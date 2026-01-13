e5ce0f85cb12be679f523403e4a8e9ce96d6ebc8
Rabb Khan <rabbilkhan125@gmail.com>
2026-01-14 02:25:26 +0530

    Fix: import Outlet in RecruiterRoute to fix live server error

--- frontend/src/pages/auth/RecruiterRoute .jsx
+++ frontend/src/pages/auth/RecruiterRoute .jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const RecruiterRoute  = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
